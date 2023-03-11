03f3ade5e8f95e6e830577ce50c379b014c03098
Nicholas Galante <nicholasgalante94@gmail.com>
2023-03-11 12:12:11 -0500

    commit tree 0280b58f38adfaec01ed4dc9d675ec8ddd975212

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map { |n| n.first_name + " " + n.last_name }
+  end
 end
